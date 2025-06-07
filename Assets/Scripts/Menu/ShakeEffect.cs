23dc69870b73e5036db9e1cf8303a61362bee041
MeoSuytGay <hunghqde170582@fpt.edu.vn>
2025-06-07 22:02:14 +0700

    update Menu , add audio, new font style

---
+++ Assets/Scripts/Menu/ShakeEffect.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using TMPro;
+
+public class TextShake : MonoBehaviour
+{
+    public float duration = 0.5f;     // Thời gian rung
+    public float magnitude = 5f;      // Độ mạnh của rung
+
+    private Vector2 originalPos;
+    private RectTransform rectTransform;
+
+    void Awake()
+    {
+        rectTransform = GetComponent<RectTransform>();
+        originalPos = rectTransform.anchoredPosition;
+    }
+
+    public void Shake()
+    {
+        StopAllCoroutines();
+        StartCoroutine(ShakeCoroutine());
+    }
+
+    private System.Collections.IEnumerator ShakeCoroutine()
+    {
+        float elapsed = 0f;
+
+        while (elapsed < duration)
+        {
+            float offsetX = Random.Range(-1f, 1f) * magnitude;
+            float offsetY = Random.Range(-1f, 1f) * magnitude;
+
+            rectTransform.anchoredPosition = originalPos + new Vector2(offsetX, offsetY);
+
+            elapsed += Time.deltaTime;
+            yield return null;
+        }
+
+        rectTransform.anchoredPosition = originalPos;
+    }
+}
